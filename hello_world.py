ef026b37f1ab6abe36b562273d15c1a0e023a1ba
Jyoti Bujethiya <jyotibujethiya22@gmail.com>
2021-02-18 22:06:26 +0530

    python basics added

--- hello_world.py
+++ hello_world.py
@@ -12,3 +12,32 @@ print(f"value of a is: {a}")
 
 # equivalent cpp code
 # cout<<"value of a is:"<<a<<endl;
+# relational: <,>,<=,>=
+
+print(a**2)
+# cout<< pow(a,2)<<endl;
+
+#logical and, or, not
+#cpp comments // py me #
+a=17
+b=15
+c=16
+
+maximum = -1
+#py me indentation is strictly required
+if a > b and a > c:
+    maximum = a
+else:
+    maximum = b
+print(maximum)
+
+"""
+if (a>b && a>c){
+    maximum=a;
+}
+"""
+#loops
+counter = 0
+while counter < 10:
+    print(counter)
+    counter += 1
